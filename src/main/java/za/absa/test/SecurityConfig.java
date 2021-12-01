b2de1fb8c0574d2e74d2d96fe48198d4347e063f
Fhatuwani Rambau <rfhatu@hotmail.com>
2021-12-01 10:00:08 +0200

    Update SecurityConfig.java

--- src/main/java/za/absa/test/SecurityConfig.java
+++ src/main/java/za/absa/test/SecurityConfig.java
@@ -8,6 +8,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .antMatchers("/").permitAll()
+        .antMatchers("/order").authenticated()
+        .antMatchers("/orders").authenticated()
         .anyRequest().authenticated()
         .and()
         .oauth2Login();
