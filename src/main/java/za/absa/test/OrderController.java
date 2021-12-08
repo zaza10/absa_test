1f3b5026c53f47cc62c9cea5f27479dc1faa20f5
Fhatuwani Rambau <fhatuwani.ramb au@partner.bmw.co.za>
2021-12-08 23:11:21 +0200

    wip

--- src/main/java/za/absa/test/OrderController.java
+++ src/main/java/za/absa/test/OrderController.java
@@ -2,8 +2,12 @@ package za.absa.test;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,5 +34,9 @@ public class OrderController {
   public String healthCheck(){
     ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
     return responseEntity.getStatusCode().toString();
+  }
+  @GetMapping("/user")
+  public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+    return Collections.singletonMap("name", principal.getAttribute("name"));
   }
 }
