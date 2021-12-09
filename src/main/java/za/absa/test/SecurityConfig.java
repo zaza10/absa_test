6c60ff3b9f50de4784b84b4eec3298b5552e3683
Fhatuwani Rambau <fhatuwani.ramb au@partner.bmw.co.za>
2021-12-09 10:09:44 +0200

    wip

--- src/main/java/za/absa/test/SecurityConfig.java
+++ src/main/java/za/absa/test/SecurityConfig.java
@@ -1,5 +1,6 @@
 package za.absa.test;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +10,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
     http.authorizeRequests()
         .antMatchers("/").permitAll()
         .antMatchers("/create-order").permitAll()
         .antMatchers("/find-all").authenticated()
-//        .anyRequest().authenticated()
+        .anyRequest().authenticated()
         .and()
         .oauth2Login();
   }
